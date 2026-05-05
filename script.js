cdb704794b451889b6fbbed1b0228fca4574c37c
riggen212 <bastiriggen212@gmail.com>
2026-05-05 21:28:15 +0200

    JSON with questions and answers added show question plus answers in card added

---
+++ script.js
@@ -0,0 +1,75 @@
+let questions = [
+    {
+        "question": "Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Was bedeutet das HTML Tag &lt;a&gt;?",
+        "answer_1": "Text Fett",
+        "answer_2": "Container",
+        "answer_3": "Ein Link",
+        "answer_4": "Kursiv",
+        "right_answer": 3
+    },
+    {
+        "question": "Wie bindet man eine Website in eine Website ein?",
+        "answer_1": "&lt;iframe&gt;, &lt;frame&gt;, and &lt;frameset&gt;",
+        "answer_2": "&lt;iframe&gt;",
+        "answer_3": "&lt;frame&gt;",
+        "answer_4": "&lt;frameset&gt;",
+        "right_answer": 3
+    },
+    {
+        "question": "Wer hat HTML erfunden",
+        "answer_1": "Robbie Williams",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3
+    },
+    {
+        "question": "Welches Attribut kann man NICHT für Textarea verwenden?",
+        "answer_1": "readonly",
+        "answer_2": "max",
+        "answer_3": "from",
+        "answer_4": "spellcheck",
+        "right_answer": 1
+    },
+    {
+        "question": "Wie wählst du alle Elemente vom Typ &lt;a&gt; mit dem attribute title aus?",
+        "answer_1": "a[title]{...}",
+        "answer_2": "a > title {...}",
+        "answer_3": "a.title {...}",
+        "answer_4": "a=title {...}",
+        "right_answer": 1
+    },
+    {
+        "question": "Wie definiert man in JavaScript eine Variable?",
+        "answer_1": "let 100 = rate;",
+        "answer_2": "100 = let rate;",
+        "answer_3": "rate = 100;",
+        "answer_4": "let rate = 100;",
+        "right_answer": 4
+    }  
+];
+
+let currentQuestion = 0;
+
+function init() {
+    document.getElementById('all-questions').innerHTML = questions.length;
+    showQuestion();
+}
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+    document.getElementById('currentQuestion').innerHTML = question['question'];
+    
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
+}
